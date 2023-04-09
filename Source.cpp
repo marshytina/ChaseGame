102e0d8a1ac797fb13c4b703b081902e935c17b6
marshytina <125360544+marshytina@users.noreply.github.com>
2023-04-09 21:53:59 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include "tmp.h"
+
+
+#include "Characters.h"
+#include "Arena.h"
+
+using namespace std;
+int main() {
+    system("chcp 65001");
+
+    srand(time(NULL));
+
+    Prey prey("prey", Point2D(5, 20), true);
+    Predator predator("predator", Point2D(3, 10), true);
+
+    Arena arena(30, 30, &prey, &predator);
+
+    cout << arena;
+
+    for (int i = 0; i <= 40; i++) {
+        arena.clearStep();
+
+        Point2D prevPreyLocation(prey.getLocation());
+        Point2D prevPredLocation(predator.getLocation());
+
+        prey.autoMove();
+        predator.autoMove();
+
+        if (arena.checkOverRun())
+        {
+            prey.moveTo(prevPreyLocation);
+            predator.moveTo(prevPredLocation);
+        }
+
+
+        cout << arena;
+
+        system("timeout /t 1");
+
+        system("cls");
+    }
+
+
+    //
+    //cout << "Enter a name for your prey: ";    
+    //cin >> prey_name;
+    //cout << "Enter a name for your predator: ";
+    //cin >> predator_name;
+    //
+    //cout << "За кого хотите играть? (0-жертва, 1-хищник)" << endl;
+    //cin >> sideChoice;
+
+    //bool preyNpc = true;
+    //if (sideChoice == 0) preyNpc = false;
+
+    //Prey prey(prey_name, Point2D(1, 1), preyNpc);
+    //Predator predator(predator_name, Point2D(7, 1), !preyNpc);
+
+    //prey.autoMove();
+    //predator.autoMove();
+
+
+    //Arena arena(10, 10, &prey, &predator);
+    //cout << arena << endl;
+
+    //while (1) {
+    //    
+    //    if (sideChoice == 0) {
+    //        if (check(prey, predator)) {
+    //            
+    //            predator.MoveTo(prey.getx(), prey.gety());
+    //            
+    //            cout << arena;
+    //            cout << "\n!!!!!! ХИЩНИК ПОБЕДИЛ !!!!!!" << endl;
+    //            return 0;
+    //        }
+    //        else {
+    //            prey.AutoMove(arena, sideChoice);
+    //            predator.AutoMove(arena, sideChoice);
+    //            cout << arena << endl;
+    //        }
+    //    }
+    //    
+    //    else {
+    //        if (check1(prey, predator)) {
+    //            cout << "\n!!!!!! ХИЩНИК ПОБЕДИЛ !!!!!!" << endl;
+    //            return 0;
+    //        }
+    //        else {
+    //            prey.AutoMove(arena, sideChoice);
+    //            predator.AutoMove(arena, sideChoice);
+    //            cout << arena << endl;
+    //        }
+    //    }
+    //}
+
+    return 0;
+}
